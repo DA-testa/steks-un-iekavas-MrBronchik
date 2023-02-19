9c1ac0c939a652a1c93de1338e438749d1ec6170
MrBronchik <sasha.mrbronchik@gmail.com>
2023-02-19 14:13:40 +0200

    lab1

--- main.py
+++ main.py
@@ -4,28 +4,48 @@ from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
-
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
-
 
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
-
+            opening_brackets_stack.append(Bracket(next, i+1))
+        elif next in ")]}":
+            if not opening_brackets_stack:
+                return i+1
+            top = opening_brackets_stack.pop()
+            if not are_matching(top.char, next):
+                return i+1
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+    else:
+        return "Success"
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    input_type = input("Enter F to use test files, or I to input brackets: ")
+    if input_type.isdigit():
+        file_num = str(input_type)
+        input_file_name = f"test/{file_num}"
+        output_file_name = f"{file_num}.a"
+    elif input_type == "F":
+        input_file_name = f"test/{file_num}"
+        output_file_name = f"{file_num}.a"
+    elif input_type == "I":
+        text = input("Enter brackets: ")
+        result = find_mismatch(text)
+        print(result)
+        return
+    else:
+        print("Invalid input type.")
+        return
 
+    with open(input_file_name, "r") as input_file, open(output_file_name, "w") as output_file:
+        text = input_file.read().strip()
+        mismatch = find_mismatch(text)
+        output_file.write(str(mismatch))
+        print(mismatch)
 
 if __name__ == "__main__":
     main()
